024c5a3022aa9ab6094a54338c0d156ed076c2db
sdarwin <samuel.d.darwin@gmail.com>
2021-05-27 19:22:48 +0000

    test

--- .drone/drone.bat
+++ .drone/drone.bat
@@ -9,19 +9,21 @@ echo "============> SCRIPT"
 
 echo "Running tests"
 True
-if !errorlevel! neq 0 exit !errorlevel!
+if !errorlevel! neq 0 exit /b !errorlevel!
 
 echo "Running libs/beast/example"
 True
 False
-if !errorlevel! neq 0 exit !errorlevel!
+if !errorlevel! neq 0 exit /b !errorlevel!
 
 echo "Running run-fat-tests"
 True
 False
-if !errorlevel! neq 0 exit !errorlevel!
+if !errorlevel! neq 0 exit /b !errorlevel!
 
 REM echo "============> COMPLETED"
 
 )
 
+echo "errorlevel is !errorlevel!"
+echo "Outside of if statement"
