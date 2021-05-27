b76a99b2ffa32eacd634d0690baaea4665ce81f6
sdarwin <samuel.d.darwin@gmail.com>
2021-05-27 19:10:33 +0000

    test

--- .drone/drone.bat
+++ .drone/drone.bat
@@ -9,19 +9,21 @@ echo "============> SCRIPT"
 
 echo "Running tests"
 True
-if !errorlevel! neq 0 exit /b 1
+if !errorlevel! neq 0 exit 1
 
 echo "Running libs/beast/example"
 True
 False
-if !errorlevel! neq 0 exit /b 1
+if !errorlevel! neq 0 exit 1
 
 echo "Running run-fat-tests"
 True
 False
-if !errorlevel! neq 0 exit /b 1
+if !errorlevel! neq 0 exit 1
 
 REM echo "============> COMPLETED"
 
 )
+
+False
 
