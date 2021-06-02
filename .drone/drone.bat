2f27e616c819f9409fd9dee540d5431d408d5a8e
sdarwin <samuel.d.darwin@gmail.com>
2021-06-02 18:52:50 +0000

    test

--- .drone/drone.bat
+++ .drone/drone.bat
@@ -28,7 +28,6 @@ if !errorlevel! neq 0 exit /b !errorlevel!
 
 echo "Running libs/beast/example"
 b2 --debug-configuration variant=%VARIANT% cxxstd=%CXXSTD% define=%DEFINE% address-model=%ADDRESS_MODEL% toolset=%TOOLSET% libs/beast/example -j3
-False
 if !errorlevel! neq 0 exit /b !errorlevel!
 
 echo "Running run-fat-tests"
