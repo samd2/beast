57761f35311187426c54ed3899301824659c168d
sdarwin <samuel.d.darwin@gmail.com>
2022-10-03 06:42:43 -0600

    further testing s390x

--- .drone/drone.sh
+++ .drone/drone.sh
@@ -18,7 +18,8 @@ export TRAVIS_BRANCH=$DRONE_BRANCH
 export TRAVIS_EVENT_TYPE=$DRONE_BUILD_EVENT
 
 common_install () {
-  git clone https://github.com/boostorg/boost-ci.git boost-ci-cloned --depth 1
+  # git clone https://github.com/boostorg/boost-ci.git boost-ci-cloned --depth 1
+  git clone -b s390x https://github.com/samd2/boost-ci.git boost-ci-cloned --depth 1
   cp -prf boost-ci-cloned/ci .
   rm -rf boost-ci-cloned
 
